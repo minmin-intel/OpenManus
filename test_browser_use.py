f95413360370fe86933001d845d3df5d9e0cf5b6
minmin-intel <minmin.hou@intel.com>
2025-03-25 22:54:43 +0000

    use manus to test browser use, browser agent alwaus stuck

--- test_browser_use.py
+++ test_browser_use.py
@@ -1,14 +1,15 @@
 import asyncio
 
 from app.agent.browser import BrowserAgent
+from app.agent.manus import Manus
 from app.logger import logger
 
 
 async def main():
-    agent = BrowserAgent()
-    task = "In the Scikit-Learn July 2017 changelog, what other predictor base command received a bug fix? Just give the name, not a path."
+    agent = Manus()
+    # task = "In the Scikit-Learn July 2017 changelog, what other predictor base command received a bug fix? Just give the name, not a path."
     task = "The photograph in the Whitney Museum of American Art's collection with accession number 2022.128 shows a person holding a book. Which military unit did the author of this book join in 1813? Answer without using articles."
-    task = "Weather in San Francisco March 1, 2025"
+    # task = "Weather in San Francisco March 1, 2025"
     await agent.run(task)
 
 
