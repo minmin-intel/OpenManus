bc45045e586c27347fe3641c90b439ca7cc0c1df
minmin-intel <minmin.hou@intel.com>
2025-03-25 21:50:08 +0000

    test browser use

--- test_browser_use.py
+++ test_browser_use.py
@@ -6,20 +6,10 @@ from app.logger import logger
 
 async def main():
     agent = BrowserAgent()
-    # try:
-    #     prompt = input("Enter your prompt: ")
-    #     if not prompt.strip():
-    #         logger.warning("Empty prompt provided.")
-    #         return
-
-    #     logger.warning("Processing your request...")
-    #     await agent.run(prompt)
-    #     logger.info("Request processing completed.")
-    # except KeyboardInterrupt:
-    #     logger.warning("Operation interrupted.")
-    prompt = "What fruits are in the 2008 painting Embroidery from Uzbekistan"
-    prompt = "Weather tomorrow in San Francisco"
-    await agent.run(prompt)
+    task = "In the Scikit-Learn July 2017 changelog, what other predictor base command received a bug fix? Just give the name, not a path."
+    task = "The photograph in the Whitney Museum of American Art's collection with accession number 2022.128 shows a person holding a book. Which military unit did the author of this book join in 1813? Answer without using articles."
+    task = "Weather in San Francisco March 1, 2025"
+    await agent.run(task)
 
 
 if __name__ == "__main__":
